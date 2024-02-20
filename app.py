579e60007e2de7e8540017b5a6c6e8f0f0e3b70c
bixbyp <159828453+bixbyp@users.noreply.github.com>
2024-02-20 08:56:53 -0600

    updates to classes and app files

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from markupsafe import escape
 import os
+import flask_class as fc
+
 
 app = Flask(__name__)
 
@@ -13,10 +15,14 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route(f"/user")
+@app.route("/user")
 def show_user_profile():
     print(os.getlogin())
     return f'User {escape(os.getlogin())}'
+
+@app.route("/cabinet_entry")
+def cabinet_entry():
+    return render_template('cabinet.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
